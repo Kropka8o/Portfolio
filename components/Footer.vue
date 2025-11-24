75faa494c710820f81747c228fb8c0ac9b1075d1
Kropka8o <kropka.koniec.switata@gmail.com>
2025-11-24 15:55:34 +0100

    Added header and footer.

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+      class="flex justify-between items-center py-3 px-6 bg-(--primary)"
+  >
+    <div class="flex justify-between w-full">
+      <div class="w-7/8">
+        <p>&copy; 2025 Cheryl</p>
+      </div>
+      <div class="flex justify-between w-1/8">
+        <Button>
+          content="Kitties"
+        </Button>
+        <Button>
+          content="Github"
+        </Button>
+        <Button>
+          content="LinkedIn"
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,24 @@
-<template>
-  <nav class="bg-red-200">
-    <NuxtLink to="/" class="navbar-brand">Test</NuxtLink>
-  </nav>
-  <nuxt-page />
+<template >
+  <div class="min-h-screen grid grid-cols-1 grid-rows-[auto_1fr_auto] gap-0">
+    <div>
+      <header>
+        <Header />
+    </header>
+    </div>
+    <div
+        :style="{'background-color': '#FEECF6'}"
+        class=""
+    >
+      <div class="wrapper flex justify-center items-center">
+        <nuxt-page />
+      </div>
+    </div>
+    <div class="">
+      <footer class="bg-pink-200">
+        <Footer />
+      </footer>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
