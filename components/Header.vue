c3997bfba5ff478cfbcfd6e8981aa5158a480d1f
Kropka8o <kropka.koniec.switata@gmail.com>
2025-11-26 11:59:32 +0100

    Fixed the link prop to linkURL in button component implementations within the header.

--- components/Header.vue
+++ components/Header.vue
@@ -5,19 +5,19 @@
     <img src="https://i.pinimg.com/736x/2e/6a/ca/2e6aca416a3488415671dbe4f43c9049.jpg" class="size-10" />
   <nav class="flex justify-between items-center w-2/5 font-bold">
     <Button
-        link="/"
+        linkURL=""
         content="Home"
         bgColour="#a2f4fd" />
     <Button
-      link="/projects"
+      linkURL="/projects"
       content="Projects"
       bgColour="#96f7e4" />
     <Button
-        link="/about_me"
+        linkURL="/about_me"
         content="About Me"
         bgColour="#a3b3ff" />
     <Button
-        link="/pets"
+        linkURL="/pets"
         content="Pets"
         bgColour="#c4b4ff" />
   </nav>
